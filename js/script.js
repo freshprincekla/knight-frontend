70deb0c94fbe33a41bdcf585d7857b45f71ccc4a
Brian Serumaga <robinahkasoma@Robinahs-iMac.fios-router.home>
2018-08-30 16:12:09 -0400

    Refactored code.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const chessColumns = ['a','b','c','d','e','f','g','h'];// an array consisting of all the rows
 let wrapper = document.getElementById('wrapper');
+let getPath = document.getElementById('getPath');
+let clearButton = document.getElementById('clearButton');
+let start = document.getElementById('start');
+let end = document.getElementById('end');
 
 for(let i=1; i < 9; i++){
     let row = document.createElement('tr');
@@ -16,8 +20,6 @@ document.addEventListener('click', event => {
     //Attached an event listener to the document element and listen for the required click
     if ( event.target.classList.contains('item') ) {
         let squareName = event.toElement.id;
-        let start = document.getElementById('start');
-        let end = document.getElementById('end');
         if(!start.innerText){
             start.innerText = squareName;
             document.getElementById(squareName).classList.add("red");
@@ -28,23 +30,40 @@ document.addEventListener('click', event => {
     }
 }, false);
 
-let getPath = document.getElementById('getPath');
 getPath.addEventListener('click', async function(event){
-    let start = document.getElementById('start').innerText;
-    let end = document.getElementById('end').innerText;
-    if(start && end){
-        const res = await fetch(`http://127.0.0.1:5000/apiv1/shortestpath?start=${start}&end=${end}`);
+    let startValue = start.innerText;
+    let endValue = end.innerText;
+    if(startValue && endValue){
+        const res = await fetch(`https://knight-shortest.herokuapp.com/apiv1/shortestpath?start=${startValue}&end=${endValue}`);
         let json = await res.json();
         json = json['path']
-        console.dir(json)
         let result = document.getElementById('path')
+        result.innerHTML = '';
         Object.keys(json).map(square => {
             let div = document.createElement('div');
-            div.innerHTML = '<div>'+ square + ': ' + json[square] + '</div></br>';
+            div.innerHTML = `<div>Step ${square}:  ${json[square]}</div></br>`;
             result.appendChild(div);
         });
     }else {
-        alert('asdfasdf')
+        alert('You have not selected the start and stop square.')
     }
 })
 
+clearButton.addEventListener('click', event =>{
+    location.reload();
+});
+
+
+document.addEventListener('touch', event => {
+    //this was created as a temporary solution to handle touch events on a phone
+    if ( event.target.classList.contains('item') ) {
+        let squareName = event.toElement.id;
+        if(!start.innerText){
+            start.innerText = squareName;
+            document.getElementById(squareName).classList.add("red");
+        } else if(!end.innerText) {
+            end.innerText = squareName;
+            document.getElementById(squareName).classList.add("red");
+        }
+    }
+}, false);
